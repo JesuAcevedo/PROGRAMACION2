740b87126ef58a1a81f68e0fca2b64587debac05
JesuAcevedo <160240903+JesuAcevedo@users.noreply.github.com>
2024-02-29 20:36:14 -0500

    Add files via upload

---
+++ class persona.py
@@ -0,0 +1,16 @@
+def main():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    correo = input("Ingrese su correo electrónico: ")
+    telefono = input("Ingrese su número de teléfono: ")
+    cedula = input("Ingrese su número de cédula: ")
+  
+    print("\nDatos ingresados:")
+    print("Nombre:", nombre)
+    print("Apellido:", apellido)
+    print("Correo electrónico:", correo)
+    print("Teléfono:", telefono)
+    print("Cédula:", cedula)
+
+if __name__ == "__main__":
+    main()
